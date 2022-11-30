084caae2ed6cdf83ca6a8f2e0ab7600023aa662b
Evandro de Souza <evandroojuniorr@hotmail.com>
2022-11-29 23:10:31 -0300

    mostrando como move

---
+++ desafio-projeto-dio/src/one/digitalinnovation/gof/strategy/ComportamentoNoormal.java
@@ -0,0 +1,11 @@
+
+package one.digitalinnovation.gof.strategy;
+
+public class ComportamentoNoormal implements Comportamento{
+
+    @Override
+    public void mover() {
+        System.out.println("movendo-se normalmente...");; 
+    }
+    
+}
